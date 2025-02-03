9b8efb61a80add354792d7298f347ba4fdb1fc72
UBSITbugtong <20093776@s.ubaguio.edu>
2025-02-03 18:20:05 +0800

    Trying to do commit repositories from pc to  github using terminal

---
+++ TestRun.java
@@ -0,0 +1,6 @@
+public class TestRun{
+    public static void main(String [] args){
+        System.out.println("Hello World");
+        System.out.println("I am trying to commit to GitHub and clone repository");
+    }
+}
