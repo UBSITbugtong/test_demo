607ba12896a613c566f18cac62c7e44c0e893f9f
UBSITbugtong <20093776@s.ubaguio.edu>
2025-02-03 18:26:28 +0800

    Update TestRun.java
    changes to make to clone repository

--- TestRun.java
+++ TestRun.java
@@ -2,5 +2,9 @@ public class TestRun{
     public static void main(String [] args){
         System.out.println("Hello World");
         System.out.println("I am trying to commit to GitHub and clone repository");
+        Respose();
+    }
+    public static void Response(){
+        System.out.prinln("I have successfully created the repository, now, I'm trying to clone the repository and commit again using terminal");
     }
 }
