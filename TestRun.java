aba2383496c7ba1194b8385941a8355b49d9e671
UBSITbugtong <20093776@s.ubaguio.edu>
2025-02-03 18:30:06 +0800

    finallizing the problem

--- TestRun.java
+++ TestRun.java
@@ -2,9 +2,12 @@ public class TestRun{
     public static void main(String [] args){
         System.out.println("Hello World");
         System.out.println("I am trying to commit to GitHub and clone repository");
-        Respose();
+        Response();
     }
     public static void Response(){
-        System.out.prinln("I have successfully created the repository, now, I'm trying to clone the repository and commit again using terminal");
+        System.out.println("I have successfully created the repository, now, I'm trying to clone the repository and commit again using terminal");
+    }
+    public static void Finishing(){
+        System.out.println("Finalizing what I have created and trying to memorizze the process to make my prgress in APPDEV faster.");
     }
 }
